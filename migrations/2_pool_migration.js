5fc2099ffcdb8f3600cd519a8c2f4f0e73c2ba1f
Jake <jakejhdz@gmail.com>
2020-11-27 21:53:18 -0800

    migration file foor pooling smart contract

--- migrations/2_pool_migration.js
+++ migrations/2_pool_migration.js
@@ -1,5 +1,8 @@
 const pool = artifacts.require("Pool");
 
 module.exports = function (deployer) {
-  deployer.deploy(pool, 100, "0x20157959ae7f2e6017ba31b6c1699826e332e1ef");
+    // this is a dummy address for now; used for testing. 
+    // the 100 represents the amount in gwei. Both of these values
+    // are sent to the constructor in the smart contract. 
+    deployer.deploy(pool, 100, "0x20157959ae7f2e6017ba31b6c1699826e332e1ef");
 };
