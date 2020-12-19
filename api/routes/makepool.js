eb05ae01b91940ff13d3356a2bffcf22d78cc49e
JapanPanda <japanxpanda@yahoo.com>
2020-12-19 15:18:21 -0800

    Updated route and wei response

--- api/routes/makepool.js
+++ api/routes/makepool.js
@@ -7,9 +7,14 @@ router.post('/', function (req, res) {
   const people = req.body.people;
   // iterate through people email each address with the amount due
   console.log(req.body);
+
+  // for making the host pay the smart contract deployment
   // get pay-to address and amount owed to deploy smart contract
   let address = '0x238283712839';
   let amount = '2';
-  res.send({ address: address, amount: amount, people: people });
+  // res.send({ address: address, amount: amount, people: people });
+
+  // for making us pay for the smart contract deployment
+  res.send({ people: people });
 });
 module.exports = router;
